b88d0710d3fa44a70dd97f5d941b8ccd46ea4e26
fmulato <fmulato@yahoo.com>
2025-02-18 12:06:34 +1300

    new features calc

--- week11/pytest_demo/tests/test_calculator.py
+++ week11/pytest_demo/tests/test_calculator.py
@@ -42,9 +42,6 @@ def test_power():
 def test_factorial():
     assert factorial(5) == 120
 
-def test_power():
-    assert power(5, 2) == 25
-
 def test_prime():
     assert prime(11)
     assert not prime(10)
