eb236f3d27bf52972bb40eca6dd37f6d4843553a
fmulato <fmulato@yahoo.com>
2025-02-24 14:38:37 +1300

    first flask code

---
+++ week12/flask_web_app/static/script.js
@@ -0,0 +1,10 @@
+document.title = "Main Page";
+document.getElementById('alertBtn').addEventListener('click', function() {
+    alert('Hello from JavaScript!');
+});
+document.getElementById('alertBtn2').addEventListener('click', function() {
+    alert('This is the Buttom 2!');
+});
+document.getElementById('alertBtn3').addEventListener('click', function() {
+    alert('This is the Buttom 3!');
+});
